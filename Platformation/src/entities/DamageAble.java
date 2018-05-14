df84c0349c8edc5626e1e89d47729f18189cd6fb
asujela232 <asujela232@PR03.hhs.win.fuhsd.org>
2018-05-14 13:47:51 -0700

    Enemies

--- Platformation/src/entities/DamageAble.java
+++ Platformation/src/entities/DamageAble.java
@@ -37,7 +37,7 @@ public interface DamageAble {
 	/**
 	 * This method replenished energy points if the energy of the object is depleted
 	 */
-	public boolean energyReplenish();
+	public void energyReplenish();
 	
 	/**
 	 * The method returns back the current Energy of the DamageAble object 
--- Platformation/src/entities/Enemies.java
+++ Platformation/src/entities/Enemies.java
@@ -1,5 +1,125 @@
 package entities;
 
-public class Enemies {
+import processing.core.PImage;
+
+public class Enemies extends Sprite implements DamageAble {
+
+	private double HP;
+	private double currentEP;
+	private double baseEP;
+	private boolean replenishing;
+
+	private double level;
+	private double attackStat;
+	private double defStat;
+	private double EXP;
+
+
+	public Enemies(PImage img, int x, int y, int w, int h, double level, double statPoints) {
+		super(img, x, y, w, h);
+		// TODO Auto-generated constructor stub
+		replenishing = false;
+		HPCalculation(statPoints);
+		defCalculation(statPoints);
+		attackCalculation(statPoints);
+	}
+	
+	
+	
+	private void HPCalculation(double statPoints)
+	{
+		HP = statPoints + (level*5);
+		
+	}
+	
+	
+	
+	private void EPCalculation(double statPoints)
+	{
+		baseEP = statPoints + (level*20);
+		currentEP = baseEP;
+	}
+	
+	
+	
+	
+	private void defCalculation(double statPoints)
+	{
+		defStat = statPoints + (level*3);
+	}
+	
+	
+	
+	private void attackCalculation(double statPoints)
+	{
+		 attackStat = statPoints + (level*10);
+		
+	}
+	
+
+	@Override
+	public double damaged(double damageTaken) {
+		// TODO Auto-generated method stub
+		
+		double damage = damageTaken*(10/(10+defStat));
+		
+		HP-=damage;
+		
+		return damage;
+	}
+
+	
+	
+	@Override
+	public void regen() {
+		// TODO Auto-generated method stub
+	}
+
+	
+	
+	@Override
+	public double getHP() {
+		// TODO Auto-generated method stub
+		return HP;
+	}
+
+	
+	
+	@Override
+	public double energyDepletion() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	
+	
+	
+	@Override
+	public void energyReplenish() {
+		// TODO Auto-generated method stub
+		
+	
+			while(currentEP<baseEP)
+			{
+				replenishing = true;
+				currentEP+=0.1;
+			}
+		
+	}
+	
+	
+
+	@Override
+	public double getEP() {
+		// TODO Auto-generated method stub
+		return currentEP;
+	}
+	
+	
+	
+	public boolean ifReplenishing()
+	{
+		return replenishing;
+	}
 
 }
