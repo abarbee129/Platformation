1c1572fc412b1bb342bee87b19a28e0b0d15ae72
asujela232 <asujela232@PR03.hhs.win.fuhsd.org>
2018-05-18 13:15:34 -0700

    Melee Enemies movement

--- Platformation/src/entities/MeleeEnemy.java
+++ Platformation/src/entities/MeleeEnemy.java
@@ -1,16 +1,70 @@
 package entities;
 
+import java.awt.Rectangle;
 import java.awt.geom.Rectangle2D;
-
 import processing.core.PImage;
+import worldGeometry.Platform;
 
 public class MeleeEnemy extends Enemies {
 	
-	private Rectangle2D hitBox;
-	
-	public MeleeEnemy( PImage img ,int lvl, int statPoint)
+	private Rectangle2D sightBox;
+	private boolean moved;
+	public MeleeEnemy( PImage img ,int x, int y, int lvl, int statPoint)
 	{
-		super(img, 85, 85, lvl, statPoint);
+		super(img, x, y, lvl, statPoint);
+		
+		sightBox = new Rectangle(x-100,y, x+100, PLAYER_HEIGHT);
+		
+	}
+	
+	public void actions(Player p, Platform plat)
+	{
+		if(sightBox.contains(p))
+		{
+			moveToPlayer(p);
+		}
+		else
+		{
+			idleWalk(plat);
+		}
+	}
+	
+	public void moveToPlayer(Player p)
+	{
+		if(p.getCenterX()+2 < this.getCenterX())
+		{
+			this.walk(-1);
+		}
+		else if(p.getCenterX()+2 > this.getCenterX())
+		{
+			this.walk(1);
+		}
+	}
+	
+	public void idleWalk(Platform p)
+	{
+		double[] vars = p.getCBoxDimensions();
+		
+		double xPlat = vars[0];
+		double width = vars[2];
+		
+		
+		if(this.getX() > xPlat+4 && !moved)
+		{
+			walk(-1);
+		}
+		else if(this.getX() < width-4 && moved)
+		{
+			walk(1);
+		}
+		else if(this.getX() <= xPlat)
+		{
+			moved = true;
+		}
+		else if(this.getX() >= width)
+		{
+			moved = false;
+		}
 		
 		
 	}
