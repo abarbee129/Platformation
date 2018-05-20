d8e8dabb2aae5c87be54fa8820a353566ef26523
alex <alex@alex-PC>
2018-05-20 10:29:49 -0700

    Added in level generation, a levels folder, and an easy to modify level.

--- Platformation/src/main/DrawingSurface.java
+++ Platformation/src/main/DrawingSurface.java
@@ -5,6 +5,11 @@ import java.awt.Rectangle;
 import java.awt.Shape;
 import java.awt.event.KeyEvent;
 import java.awt.geom.Point2D;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.CharBuffer;
 import java.util.ArrayList;
 
 import entities.Enemies;
@@ -21,6 +26,8 @@ public class DrawingSurface extends PApplet {
 	public static final int DRAWING_WIDTH = 800;
 	public static final int DRAWING_HEIGHT = 600;
 
+	public static final String fileSeparator = System.getProperty("file.separator");
+
 	private Rectangle screenRect;
 	private Booster[] booster = new Booster[4];
 
@@ -29,7 +36,7 @@ public class DrawingSurface extends PApplet {
 	private ArrayList<Platform> platforms;
 	private ArrayList<Integer> keys;
 	private MeleeEnemy e;
-	
+
 	private ArrayList<PImage> assets;
 
 	public DrawingSurface() {
@@ -38,7 +45,9 @@ public class DrawingSurface extends PApplet {
 		keys = new ArrayList<Integer>();
 		screenRect = new Rectangle(0,0,DRAWING_WIDTH,DRAWING_HEIGHT);
 		obstacles = new ArrayList<Shape>();
-		platforms = new ArrayList<Platform>();
+		platforms = new ArrayList<Platform>(); 
+		initLevel("Levels" + fileSeparator + "Level1.txt");
+		/*
 		obstacles.add(new Rectangle(200,400,400,50));
 		obstacles.add(new Rectangle(0,250,100,50));
 		obstacles.add(new Rectangle(700,250,100,50));
@@ -48,21 +57,57 @@ public class DrawingSurface extends PApplet {
 		booster[1] = new Booster(290,500);
 		booster[2] = new Booster(290,500); 
 		booster[3] = new Booster(290,500);
-		 // cool
+		 */
+		// cool
 		for(Shape s : obstacles) {
 			platforms.add(new Platform(s));
 		}
 	}
 
+	public void initLevel(String fileName) {
+		try {
+			FileReader reader = new FileReader(fileName);
+			BufferedReader bReader = new BufferedReader(reader);
+			char[] chars = null;
+			double yoff = 0;
+			double xoff = 0;
+			int rows = 24;
+			double pHeight = DRAWING_HEIGHT/rows;
 
+
+			while(bReader.ready()) {
+				chars = bReader.readLine().toCharArray();
+				for(char c : chars) {
+					if(c == '#') {
+						System.out.println(xoff);
+						obstacles.add(new Rectangle((int)xoff,(int)yoff,(int)pHeight,(int)pHeight));
+					}
+					else {
+
+					}
+					xoff+=pHeight/2;
+				}
+				yoff+= pHeight;
+				xoff = 0;
+
+
+			}
+
+
+		} 
+		catch (IOException e) {
+			e.printStackTrace();
+		} 
+
+	}
 	public void spawnNewMario() {
 		player = new Player(assets.get(0), DRAWING_WIDTH/2-Mario.MARIO_WIDTH/2,50);
 	}
-	
+
 	public void spawnNewEnemy() {
 		e = new MeleeEnemy(assets.get(1),4,250-85, 2,1);
 	}
-	
+
 	public void runMe() {
 		runSketch();
 	}
@@ -106,12 +151,14 @@ public class DrawingSurface extends PApplet {
 			}
 		}
 		for(Booster b : booster) {
-			b.draw(this);
+			if(b != null) {
+				b.draw(this);
+			}
 		}
 
 		player.draw(this);
 		e.draw(this);
-		
+
 
 		popMatrix();
 
@@ -127,11 +174,13 @@ public class DrawingSurface extends PApplet {
 
 		// check for booster collisions and accelerate
 		for (int i = 0; i < booster.length; i++) {
-			double[] dims = booster[i].getCBoxDimensions();
-			if (player.doesCollideWith(dims[0], dims[1], (int)dims[2], (int)dims[3])) {
-				player.accelerate(booster[i].getXBounceAcceleration(player.getx(), player.gety()), booster[i].getYBounceAcceleration(player.getx(), player.gety()));
+			if(booster[i]!=null) {
+				double[] dims = booster[i].getCBoxDimensions();
+				if (player.doesCollideWith(dims[0], dims[1], (int)dims[2], (int)dims[3])) {
+					player.accelerate(booster[i].getXBounceAcceleration(player.getx(), player.gety()), booster[i].getYBounceAcceleration(player.getx(), player.gety()));
+				}
+				else {}
 			}
-			else {}
 		}
 		player.act(obstacles);
 		e.act(obstacles);
